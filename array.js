c9373acc85115c49b1a5bf2745a3aa391121bc33
Arnisha-Akter <aakter191194@bscse.uiu.ac.bd>
2023-09-28 14:02:56 +0600

    resolved error

--- array.js
+++ array.js
@@ -12,4 +12,32 @@
 
 //problem grading
 
+const yourScore = 85;
+const tomScore = 66;
+const janeScore = 95;
+const peterScore = 56;
+const johnScore = 40;
+
+
+function calculateGrade(score) {
+  if (score >= 80) {
+    return 'A';
+  } else if (score >= 60) {
+    return 'B';
+  } else if (score >= 50) {
+    return 'C';
+  } else if (score >= 40) {
+    return 'D';
+  } else {
+    return 'F';
+  }
+}
+
+
+console.log(`Your grade: ${calculateGrade(yourScore)}`);
+console.log(`Tom's grade: ${calculateGrade(tomScore)}`);
+console.log(`Jane's grade: ${calculateGrade(janeScore)}`);
+console.log(`Peter's grade: ${calculateGrade(peterScore)}`);
+console.log(`John's grade: ${calculateGrade(johnScore)}`);
+
 
