554b5fbca7ced827f44dd12f743889d52072e519
Arnisha-Akter <aakter191194@bscse.uiu.ac.bd>
2023-09-28 13:44:00 +0600

    prcaticed codes

---
+++ array.js
@@ -0,0 +1,15 @@
+// var vowels =['a', 'e', 'i', 'o', 'u'];
+// console.log(vowels);
+
+// var s=['mango', 'papaya', 'banana'];
+// s.push('jackfruite');
+// s.push('bird');
+
+// console.log(s);
+
+// s.pop('s');
+// console.log(s);
+
+//problem grading
+
+
