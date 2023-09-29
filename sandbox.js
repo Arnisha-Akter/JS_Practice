9df73c1dbcebdc7afa916a979bed01bf31c5b7dc
Arnisha-Akter <aakter191194@bscse.uiu.ac.bd>
2023-09-29 14:18:08 +0600

    additional practice

---
+++ sandbox.js
@@ -0,0 +1,21 @@
+// string concatenation 
+let f1='Arnisha';
+let f2='akter';
+console.log(f1+' '+f2);
+
+console.log(f2.toUpperCase());
+
+let email='arnisha@gmail.com';
+let result=email.slice(2,10);
+console.log(result);
+let substring=email.substring(2,14);
+console.log(substring);
+
+
+const title='JS turorial';
+const blog='Net ninja';
+const viwes='100K';
+// template string
+let result2=`The video of ${title} authored by ${blog} has ${viwes} viwes` ;
+console.log(result2);
+
